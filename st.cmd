70ee1762a2ebcccb4a4f2a94768983e9926131b0
Psm715 <101235083+Psm715@users.noreply.github.com>
2022-03-16 14:09:31 -0400

    Add files via upload
    Changed location of St.cmd and edited IOC macros

---
+++ st.cmd
@@ -0,0 +1,29 @@
+#!./bin/linux-arm/test
+
+#- You may have to change test to something else
+#- everywhere it appears in this file
+
+#< envPaths
+
+#cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/test.dbd"
+test_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadTemplate "db/user.substitutions"
+dbLoadRecords "db/testVersion.db", "user=PSM"
+dbLoadRecords "db/dbSubExample.db", "user=PSM"
+
+#- Set this to see messages from mySub
+#var mySubDebug 1
+
+#- Run this to trace the stages of iocInit
+#traceIocInit
+
+#cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncExample, "user=pi"
